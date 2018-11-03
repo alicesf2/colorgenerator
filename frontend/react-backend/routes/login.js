597ff6be7893e4a412c3af110912d9c8d794f139
Polarpi <eugeniachen123@gmail.com>
2018-11-03 17:31:19 -0500

    update spotify integration with react and express

---
+++ frontend/react-backend/routes/login.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+var client_id = 'b9444c2fa5784fdf9f780f17bd16c50d'; // Your client id
+var client_secret = '3e7d5e2e5c934c3abcde124929e747b9'; // Your secret // REMOVE BEFORE PUSHING TO GITHUB
+var redirect_uri = 'http://localhost:3001/callback'; // Your redirect uri
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+var stateKey = 'spotify_auth_state';
+
+router.get('/login', function(req, res) {
+
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+
+module.exports = router;

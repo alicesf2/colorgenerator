38e994a254079ae1d0ef1181855c2d90d8876b72
juliass2 <juliass2@illinois.edu>
2018-11-03 16:57:36 -0500

    fixing up chosen colors

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     #each scheme has 3 colors
-    colors = utils.getColors(0.4, 80, 1, 1)
+    colors = utils.getColors(0.8, 80, 0, 1)
 
     return "%s, %s, %s, %s, %s" % (colors[0], colors[1], colors[2], colors[3], colors[4])
